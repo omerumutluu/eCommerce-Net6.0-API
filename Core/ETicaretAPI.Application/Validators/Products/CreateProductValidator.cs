1a39fbc9a3fdee8f4a664ff09adbb86faa673bf4
omerumutluu <omer.umutluu@icloud.com>
2022-07-12 19:57:05 +0300

    Add project files.

---
+++ Core/ETicaretAPI.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,41 @@
+﻿using ETicaretAPI.Application.ViewModels.Products;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Validators.Products
+{
+    public class CreateProductValidator : AbstractValidator<VM_Create_Product>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                    .WithMessage("Lütfen ürün adını boş geçmeyiniz.")
+                .NotNull()
+                    .WithMessage("Lütfen ürün adını boş geçmeyiniz.")
+                .MaximumLength(150)
+                .MinimumLength(5)
+                    .WithMessage("Lütfen ürün adını 5 ile 150 karekter arasında giriniz.");
+
+            RuleFor(p => p.Stock)
+                .NotEmpty()
+                    .WithMessage("Lütfen stok bilgisini boş geçmeyiniz.")
+                .NotNull()
+                    .WithMessage("lütfen stok bilgisini boş geçmeyiniz")
+                .Must(s => s >= 0)
+                    .WithMessage("Stok bilgisi negatif olamaz!");
+
+            RuleFor(p => p.Price)
+                .NotEmpty()
+                    .WithMessage("Lütfen ürün fiyatını boş geçmeyiniz.")
+                .NotNull()
+                    .WithMessage("Lütfen ürün fiyatını giriniz.")
+                .Must(s => s >= 0)
+                    .WithMessage("Fiyat bilgisi negatif olamaz!");
+        }
+    }
+}
---
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,158 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Application.RequestParameters;
+using ETicaretAPI.Application.Services;
+using ETicaretAPI.Application.ViewModels.Products;
+using ETicaretAPI.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductWriteRepository _productWriteRepository;
+        private readonly IProductReadRepository _productReadRepository;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IFileService _fileService;
+        private readonly IFileWriteRepository _fileWriteRepository;
+        private readonly IFileReadRepository _fileReadRepository;
+        private readonly IProductImageFileReadRepository _productImageFileReadRepository;
+        private readonly IProductImageFileWriteRepository _productImageFileWriteRepository;
+        private readonly IInvoiceFileWriteRepository _invoiceFileWriteRepository;
+        private readonly IInvoiceFileReadRepository _invoiceFileReadRepository;
+
+
+        public ProductsController(
+            IProductWriteRepository productWriteRepository,
+            IProductReadRepository productReadRepository,
+            IWebHostEnvironment webHostEnvironment,
+            IFileService fileService,
+            IInvoiceFileReadRepository invoiceFileReadRepository,
+            IInvoiceFileWriteRepository invoiceFileWriteRepository,
+            IFileReadRepository fileReadRepository,
+            IFileWriteRepository fileWriteRepository,
+            IProductImageFileReadRepository productImageFileReadRepository,
+            IProductImageFileWriteRepository productImageFileWriteRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+            _webHostEnvironment = webHostEnvironment;
+            _fileService = fileService;
+            _invoiceFileReadRepository = invoiceFileReadRepository;
+            _invoiceFileWriteRepository = invoiceFileWriteRepository;
+            _fileReadRepository = fileReadRepository;
+            _fileWriteRepository = fileWriteRepository;
+            _productImageFileReadRepository = productImageFileReadRepository;
+            _productImageFileWriteRepository = productImageFileWriteRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get([FromQuery] Pagination pagination)
+        {
+            var totalCount = _productReadRepository.GetAll(false).Count();
+            var products = _productReadRepository.GetAll(false).Skip(pagination.Page * pagination.Size).Take(pagination.Size).Select(p => new
+            {
+                p.Id,
+                p.Name,
+                p.Stock,
+                p.Price,
+                p.CreatedTime,
+                p.UpdatedTime
+            });
+            return Ok(
+                new
+                {
+                    totalCount,
+                    products
+                });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            return Ok(await _productReadRepository.GetByIdAsync(id, false));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(VM_Create_Product model)
+        {
+            await _productWriteRepository.AddAsync(new() { Name = model.Name, Price = model.Price, Stock = model.Stock });
+            await _productWriteRepository.SaveAsync();
+            return StatusCode((int)HttpStatusCode.Created);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(VM_Update_Product model)
+        {
+            Product product = await _productReadRepository.GetByIdAsync(model.Id);
+            product.Name = model.Name;
+            product.Stock = model.Stock;
+            product.Price = model.Price;
+            await _productWriteRepository.SaveAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var product = await _productWriteRepository.RemoveAsync(id);
+            await _productWriteRepository.SaveAsync();
+            return Ok();
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Upload()
+        {
+            //string uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, "resource/product-image");
+
+            //if (!Directory.Exists(uploadPath))
+            //    Directory.CreateDirectory(uploadPath);
+
+
+            //Random random = new();
+            //foreach (IFormFile file in Request.Form.Files)
+            //{
+            //    string fullPath = Path.Combine(uploadPath,  $"{random.Next()}{Path.GetExtension(file.FileName)}");
+            //    using FileStream fileStream = new(fullPath, FileMode.Create, FileAccess.Write, FileShare.None,1024 * 1024,false);
+            //    await file.CopyToAsync(fileStream);
+            //    await fileStream.FlushAsync();
+            //}
+            //return Ok();
+
+            var datas = await _fileService.UploadAsync("resource/invoices", Request.Form.Files);
+
+            //await _productImageFileWriteRepository.AddRangeAsync(datas.Select(x => new ProductImageFile
+            //{
+            //    FileName = x.fileName,
+            //    Path = x.path
+            //}).ToList());
+            //await _productWriteRepository.SaveAsync();
+
+            //await _invoiceFileWriteRepository.AddRangeAsync(datas.Select(x => new InvoiceFile
+            //{
+            //    FileName = x.fileName,
+            //    Path = x.path,
+            //    Price = new Random().Next()
+            //}).ToList());
+            //await _invoiceFileWriteRepository.SaveAsync();
+
+            //await _fileWriteRepository.AddRangeAsync(datas.Select(x => new Domain.Entities.File
+            //{
+            //    FileName = x.fileName,
+            //    Path = x.path
+            //}).ToList());
+            //await _fileWriteRepository.SaveAsync();
+
+            //var d1 = _fileReadRepository.GetAll(false);
+            //var d2 = _invoiceFileReadRepository.GetAll(false);
+            //var d3 = _productImageFileReadRepository.GetAll(false);
+
+            return Ok();
+        }
+
+
+    }
+}
